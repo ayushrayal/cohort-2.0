dd2a525751abf90d9000b59d89e5fc4d07bcb352
ayushrayal19@gmail.com <ayushrayal19@gmail.com>
2025-11-21 19:00:11 +0530

    Mouseover And Mouseout!

---
+++ 6-JavaSCriptDOM/13-MouseOverAndMouseOut/script.js
@@ -0,0 +1,7 @@
+let abcd = document.querySelector("div");
+abcd.addEventListener("mouseover",function(){
+    abcd.style.backgroundColor = "red";
+});
+abcd.addEventListener("mouseout",function(){
+    abcd.style.backgroundColor = "beige";
+});
