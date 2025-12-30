31313a45caafd3b6a056d9e364db933bdedab78f
ayushrayal19@gmail.com <ayushrayal19@gmail.com>
2025-12-30 10:11:37 +0530

    Advance Js Start.

---
+++ 8-AdvanceJS/1-OOPSPart1/script.js
@@ -0,0 +1,9 @@
+class human{
+    constructor(name, age , gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+let h1 = new human("ayush rayal", 21, "Male");
+let h2 = new human("geeta joshi", 22, "Female");
